c93b0bad1f8e162cf185e4e4031df2770aaedd11
Rohini Amruskar <rohiniamruskar1999@gmail.com>
2025-12-20 23:05:16 +0530

    Update system health checks

--- day-01/system_health.py
+++ day-01/system_health.py
@@ -4,15 +4,17 @@ def system_health_check():
     # Take thresholds from user
     cpu_threshold = int(input("Enter CPU usage threshold (%): "))
     memory_threshold = int(input("Enter Memory usage threshold (%): "))
+    disk_threshold = int(input("Enter Disk usage threshold (%):"))
 
     # Get current CPU and Memory usage
     current_cpu = psutil.cpu_percent(interval=1)
     current_memory = psutil.virtual_memory().percent
-   # current_memory = memory.percent
+    current_disk = psutil.disk_usage('C:\\').percent
 
     print("\n--- System Health Report ---")
     print(f"CPU Usage    : {current_cpu}%")
     print(f"Memory Usage : {current_memory}%")
+    print(f"Disk Usage : {current_disk}%")
 
     # CPU check
     if current_cpu > cpu_threshold:
@@ -27,6 +29,13 @@ def system_health_check():
         print("Memory alert sent to Admin")
     else:
         print("Memory is in safe state")
+
+    # Disk check
+    if current_disk > disk_threshold:
+        print("ALERT: Disk usage is HIGH")
+        print("Disk alert sent to Admin")
+    else:
+        print("Disk is in safe state")    
 
 # Call the function
 system_health_check()
